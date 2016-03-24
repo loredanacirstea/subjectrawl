052d7610b80277bc63b167e3682c19a9ca523703
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-03-24 22:35:52 +0200

    swagger doc for api

--- server/server.js
+++ server/server.js
@@ -7,17 +7,16 @@
  *
  * @header Strict-Transport-Security: max-age=2592000; includeSubDomains
  */
-/*
-var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implementation
 
 // attach connect-style middleware for response header injection
-Meteor.startup(function () {
-  connectHandler.use(function (req, res, next) {
-    res.setHeader('Strict-Transport-Security', 'max-age=2592000; includeSubDomains'); // 2592000s / 30 days
-    return next();
-  })
+WebApp.connectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
 });
-*/
+
+//Access-Control-Allow-Origin: http://swagger.io
+//Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept
+
 
 initCsvCollection = function(csv, Collection){
     var data = Assets.getText(csv);
